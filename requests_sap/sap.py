cb08c593e39d41b9e4e29e4a8a1992f9f533e310
Ondra Machacek <omachace@redhat.com>
2022-10-06 14:55:02 +0200

    Add universal ID support
    Signed-off-by: Ondra Machacek <omachace@redhat.com>

--- requests_sap/sap.py
+++ requests_sap/sap.py
@@ -1,8 +1,12 @@
+import json
+
 import requests
 from requests.auth import AuthBase
 
 from lxml import etree
 import xml.etree.ElementTree as ET
+
+from urllib.parse import urlparse
 
 
 class SAPAuth(AuthBase):
@@ -51,8 +55,53 @@ class SAPAuth(AuthBase):
 
         return next_response
 
+    def _get_gigya_params(self, response):
+        api_key = None
+        saml_context = None
+        for p in urlparse(response.url).query.split('&'):
+            if p.startswith('samlContext='):
+                saml_context = p[len('samlContext='):]
+            elif p.startswith('apiKey='):
+                api_key = p[len('apiKey='):]
+
+        return saml_context, api_key
+
+    def _get_login_token(self, history):
+        response = requests.post(
+            'https://core-api.account.sap.com/uid-core/authenticate?reqId=https://hana.ondemand.com/supportportal',
+            json={'login': self._username, 'password': self._password},
+            # For some reason requests/python user agent is not accepted:
+            headers={'User-Agent': 'curl/7.82.0'},
+        )
+        history.append(response)
+        data = json.loads(response.text)
+        return data['cookieValue']
+
+    def _get_saml_response(self, response, history):
+        saml_context, api_key = self._get_gigya_params(response)
+        login_token = self._get_login_token(history)
+
+        response = requests.get(
+            'https://cdc-api.account.sap.com/saml/v2.0/{0}/idp/sso/continue?loginToken={1}&samlContext={2}'.format(api_key, login_token, saml_context)
+        )
+        history.append(response)
+        return response
+
+    def _gigya(self, response, history):
+        response = self._next_step(response, history)
+        response = self._next_step(response, history, j_username=self._username)
+        response = self._next_step(response, history)
+        response = self._get_saml_response(response, history)
+        response = self._next_step(response, history)
+        response = self._next_step(response, history)
+        return self._next_step(response, history, headers=self._headers)
+
     def handle_response(self, response, **kwargs):
         history = [response]
+
+        if '@' in self._username:
+            return self._gigya(response, history)
+
         response = self._next_step(response, history)
         response = self._next_step(response, history, j_username=self._username)
         # We need to pass the next_url explicitly, because the response only contains relative URL for some reason:
